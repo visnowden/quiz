17a0e2456b0ddc6d078688605deb86a34d92b97c
Code_H29/ <72585285+Hacker-Bug-Hunter@users.noreply.github.com>
2023-09-12 02:03:18 -0300

    Funcional está, só está faltando todo o resto...

--- script.js
+++ script.js
@@ -1,75 +1,78 @@
-answerButtons = document.querySelectorAll(".answers");
-answerButtons.forEach(function (element) {
+correctAnswerButtons = document.querySelectorAll(".options")
+correctAnswerButtons.forEach(function (element) {
   element.addEventListener("click", function () {
-    answerButtons.forEach(function (elements) {
-      elements.classList.remove("clicked");
-    });
-    element.classList.add("clicked");
-  });
-});
-questionIndex = 0;
+    correctAnswerButtons.forEach(function (elements) {
+      elements.classList.remove("clicked")
+    })
+    element.classList.add("clicked")
+  })
+})
+questionIndex = 19
 function loadQuestion() {
-  questionIndex++;
-  title = document.getElementById("title");
-  question = document.getElementById("question");
-  answer1 = document.getElementById("answer1");
-  answer2 = document.getElementById("answer2");
-  answer3 = document.getElementById("answer3");
-  answer4 = document.getElementById("answer4");
-  answer5 = document.getElementById("answer5");
-  justify = document.getElementById("justify");
-  button = document.getElementById("button");
+  questionIndex++
+  title = document.getElementById("title")
+  question = document.getElementById("question")
+  optionA = document.getElementById("optionA")
+  optionB = document.getElementById("optionB")
+  optionC = document.getElementById("optionC")
+  optionD = document.getElementById("optionD")
+  optionE = document.getElementById("optionE")
+  explanation = document.getElementById("explanation")
+  button = document.getElementById("button")
   fetch("questions.json") /* Especifique o nome do arquivo JSON*/
     .then((response) => {
       if (!response.ok) {
-        throw new Error("Erro na requisição!");
+        throw new Error("Erro na requisição!")
       }
-      return response.json();
+      return response.json()
     })
     .then((data) => {
-      /* Agora você pode trabalhar com os dados como um objeto JavaScript*/ title.innerHTML = `${data[questionIndex]["id"]}. ${data[questionIndex]["title"]}`;
-      question.innerHTML = data[questionIndex]["question"];
-      answer1.innerHTML = data[questionIndex]["answer1"];
-      answer2.innerHTML = data[questionIndex]["answer2"];
-      answer3.innerHTML = data[questionIndex]["answer3"];
-      answer4.innerHTML = data[questionIndex]["answer4"];
-      answer5.innerHTML = data[questionIndex]["answer5"];
-      answer = data[questionIndex]["answer"];
-      justify.innerHTML =
+      console.log(questionIndex)
+      if(questionIndex == data.length){button.style.display = 'none';questionIndex = 0}
+      /* Agora você pode trabalhar com os dados como um objeto JavaScript*/
+      title.innerHTML = `${data[questionIndex]["id"]}. ${data[questionIndex]["title"]}`
+      question.innerHTML = data[questionIndex]["question"]
+      optionA.innerHTML = data[questionIndex]["optionA"]
+      optionB.innerHTML = data[questionIndex]["optionB"]
+      optionC.innerHTML = data[questionIndex]["optionC"]
+      optionD.innerHTML = data[questionIndex]["optionD"]
+      optionE.innerHTML = data[questionIndex]["optionE"]
+      correctAnswer = data[questionIndex]["correctAnswer"]
+      explanation.innerHTML =
         data[questionIndex][
-          "justify"
-        ]; /* Por exemplo, você pode acessar propriedades do objeto JSON*/ /*console.log(data.id)*/ /* Ou iterar sobre arrays de objetos JSON*/ /* data.forEach(item => {*/ /*   console.log(item.id, item.title)*/ /* })*/
+          "explanation"
+        ] /* Por exemplo, você pode acessar propriedades do objeto JSON*/ /*console.log(data.id)*/ /* Ou iterar sobre arrays de objetos JSON*/ /* data.forEach(item => {*/ /*   console.log(item.id, item.title)*/ /* })*/
     })
     .catch((error) => {
-      console.error(error);
-    });
+      console.error(error)
+    })
   button.onclick = function () {
-    var element = document.querySelector(".clicked");
-    var logs = document.getElementById("logs");
+    var element = document.querySelector(".clicked")
+    var logs = document.getElementById("logs")
     if (element !== null) {
-      if (element.id == answer) {
-        msg(logs, "Boa Truta!!!");
-        button.textContent = "Próxima";
-        justify.style.display = "flex";
+      if (element.id == correctAnswer) {
+        msg(logs, "Boa Truta!!!")
+        button.textContent = "Próxima"
+        explanation.style.display = "flex"
         button.onclick = function () {
-          loadQuestion();
-          button.textContent = "Confirmar";
-          justify.style.display = "none";
-          element.classList.remove("clicked");
-        };
+          loadQuestion()
+          button.textContent = "Confirmar"
+          explanation.style.display = "none"
+          element.classList.remove("clicked")
+        }
       } else {
-        msg(logs, "Errou Vacalo!!!");
+        msg(logs, "Errou Vacalo!!!")
       }
     } else {
-      msg(logs, "Por favor, escolha uma alternativa!");
+      msg(logs, "Por favor, escolha uma alternativa!")
     }
-  };
+  }
 }
 function msg(element, text) {
-  element.innerText = text;
-  element.classList.remove("hidden");
+  element.innerText = text
+  element.classList.remove("hidden")
   setTimeout(function () {
-    element.classList.add("hidden");
-  }, 2000);
+    element.classList.add("hidden")
+  }, 2000)
 }
-loadQuestion();
+loadQuestion()
